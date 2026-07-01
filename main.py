d7bbb682fca3b51b488b13c6a27bcd6b2372d72f
Parth Lamba <parthlamba2007@gmail.com>
2026-07-02 00:13:57 +0530

    Added jarvis
    Implement voice assistant features with speech recognition and web integration.

---
+++ main.py
@@ -0,0 +1,121 @@
+import speech_recognition as sr
+import webbrowser
+import pyttsx3
+import musicLibrary
+import requests
+from gtts import gTTS
+import pygame
+import os
+
+
+recognizer = sr.Recognizer()
+engine = pyttsx3.init() 
+newsapi = "b2c61553094e4728b96b45de2710416c"
+
+def speak_old(text):
+    engine.say(text)
+    engine.runAndWait()
+
+# Initialize Pygame mixer
+pygame.mixer.init()
+
+def speak(text):
+    tts = gTTS(text)
+
+    #Saving the mp3 file
+    tts.save("temp.mp3")
+
+    # Load the MP3 file
+    pygame.mixer.music.load('temp.mp3')
+
+    # Play the MP3 file
+    pygame.mixer.music.play()
+
+    # Keep the program running until the music stops playing
+    while pygame.mixer.music.get_busy():
+        pygame.time.Clock().tick(10)
+    
+    pygame.mixer.music.unload()
+    os.remove("temp.mp3") 
+
+'''def aiProcess(command):
+    client = OpenAI(api_key="<Your Key Here>",
+    )
+
+    completion = client.chat.completions.create(
+    model="gpt-3.5-turbo",
+    messages=[
+        {"role": "system", "content": "You are a virtual assistant named jarvis skilled in general tasks like Alexa and Google Cloud. Give short responses please"},
+        {"role": "user", "content": command}
+    ]
+    )
+
+    return completion.choices[0].message.content'''
+
+def processCommand(c):
+    if "open google" in c.lower():
+        webbrowser.open("https://google.com")
+    elif "open instagram" in c.lower():
+        webbrowser.open("https://instagram.com")
+    elif "open youtube" in c.lower():
+        webbrowser.open("https://youtube.com")
+    elif "open linkedin" in c.lower():
+        webbrowser.open("https://linkedin.com")
+    elif "open whatsapp" in c.lower():
+        webbrowser.open("https://web.whatsapp.com/")
+    elif c.lower().startswith("play"):
+        song = " ".join(c.lower().split()[1:])
+        link = musicLibrary.music[song]
+        webbrowser.open(link)
+
+    elif "news" in c.lower():
+        r = requests.get(f"https://newsapi.org/v2/top-headlines?country=in&apiKey={newsapi}", timeout = 5)
+        if r.status_code == 200:
+            # Parse the JSON response
+            data = r.json()
+            
+            # Extract the articles
+            articles = data.get('articles', [])
+            
+            # Print the headlines
+            for article in articles:
+                speak(article['title'])
+
+    else:
+        # Error 404
+        webbrowser.open("https://www.awwwards.com/awwwards/collections/404-error-page/")
+         
+
+
+
+
+
+if __name__ == "__main__":
+    speak("Initializing Jarvis....")
+    while True:
+        # Listen for the wake word "Jarvis"
+        # obtain audio from the microphone
+        r = sr.Recognizer()
+         
+        print("recognizing...")
+        try:
+            with sr.Microphone() as source:
+                print("Listening...")
+                audio = r.listen(source, timeout=2, phrase_time_limit=2)
+            word = r.recognize_google(audio)
+            if(word.lower() == "jarvis"):
+                speak("Ya")
+                # Listen for command
+                with sr.Microphone() as source:
+                    print("Jarvis Active...")
+                    audio = r.listen(source)
+                    command = r.recognize_google(audio)
+
+                    print(command)
+                    processCommand(command)
+
+
+        except Exception as e:
+            print(f"Error: {e}")
+
+
